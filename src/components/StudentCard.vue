ef19b2c1b6edca04813e74d4799fd969dde42ff6
wombat1742 <tomgogard@gmail.com>
2024-12-13 10:58:45 +0300

    first prototype

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,40 +1,48 @@
 <template>
-  <div
-    class="rounded"
-    v-touch-swipe.left="$emit('on_left_action')"
-    v-touch-swipe.right="'on_right_action'"
+  <q-slide-item
+    class="q-slide-item"
+    left-color="green"
+    right-color="red"
+    @left="$emit('on_left_action')"
+    @right="$emit('on_right_action')"
   >
-    <q-btn
-      size="sm"
-      round
-      flat
-      icon="fa-solid fa-chevron-left"
-      class="qbtn success"
-      @click="$emit('on_left_action')"
-    />
-    <div class="">{{ student.name }}</div>
-    <q-btn
-      size="sm"
-      round
-      flat
-      icon="fa-solid fa-chevron-right"
-      class="qbtn danger"
-      @click="$emit('on_right_action')"
-    />
-  </div>
+    <q-item class="flex">
+      <q-btn
+        size="sm"
+        round
+        flat
+        icon="fa-solid fa-chevron-left"
+        class="qbtn success"
+        @click="$emit('on_left_action')"
+      />
+      <div class="">{{ name }}</div>
+      <q-btn
+        size="sm"
+        round
+        flat
+        icon="fa-solid fa-chevron-right"
+        class="qbtn danger"
+        @click="$emit('on_right_action')"
+      />
+    </q-item>
+    <template v-slot:left> <q-icon name="done" /></template>
+    <template v-slot:right><q-icon name="cancel" /></template>
+  </q-slide-item>
 </template>
 <script setup>
 import { defineProps } from "vue";
-import { QBtn } from "quasar";
+
+import { QBtn, QSlideItem, QItem } from "quasar"; // Slide Element
 //import { QMenu, QList, QItem } from "quasar"; // menu
 
 const props = defineProps({
-  student: Object,
+  id: Number,
+  name: String,
   type: String,
 });
 const emits = defineEmits(["on_left_action", "on_right_action"]);
 const icons = {
-  are_present: {
+  is_present: {
     left: "",
     right: "",
   },
@@ -42,7 +50,7 @@ const icons = {
     left: "",
     right: "",
   },
-  arent_present: {
+  is_not_present: {
     left: "",
     right: "",
   },
@@ -51,11 +59,10 @@ const icons = {
 <style lang="css" scoped>
 .rounded {
   border: solid var(--border) 2px;
-  border-radius: 10px;
   display: flex;
   min-height: 45px;
   padding: 7px;
-  max-width: 60%;
+  width: 100%;
   margin-block: 1em;
   background-color: var(--primary);
   justify-content: space-between;
@@ -64,6 +71,12 @@ const icons = {
 .success {
   color: var(--success);
   border-color: var(--success);
+}
+.flex {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  height: 100%;
 }
 .danger {
   color: var(--danger);
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,30 +1,62 @@
 <template class="page">
-  <StudentCard
-    v-for="student of students"
-    :student="student"
-    :key="student.id"
-  />
+  <q-tabs v-model="tab" no-caps class="text-teal">
+    <q-tab label="Отсутствующие" name="is-not-present" />
+    <q-tab label="Неназначенные" :name="'undefined'" />
+    <q-tab label="Присутствующие" name="is-present" />
+  </q-tabs>
+
+  <div v-for="item of current_list" :key="item.id">
+    <StudentCard
+      :name="item.name"
+      @on_left_action="onLeft(item)"
+      @on_right_action="onRight(item)"
+    />
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, ref, reactive, onMounted } from "vue";
 import StudentCard from "src/components/StudentCard.vue";
-//
 
-const students = ref([
+const students = reactive([
   {
     id: 1,
     name: "Андреев Сергей",
+    status: "undefined",
   },
   {
     id: 2,
     name: "Бобровский Кирилл",
+    status: "undefined",
   },
   {
     id: 3,
     name: "Владимиров Захар",
+    status: "undefined",
+  },
+  {
+    id: 4,
+    name: "",
   },
 ]);
+var i = 0; //update i
+const tab = ref("undefined");
+
+function onRight(value) {
+  value.status = "is-not-present";
+  i++;
+}
+
+function onLeft(value) {
+  value.status = "is-present";
+  i--;
+}
+const current_list = computed(() => {
+  i;
+  return students.filter((i) => i.status == tab.value);
+});
+onMounted(() => {});
+console.log(current_list);
 </script>
 <style lang="css" scoped>
 .page {
